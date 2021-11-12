da6a91f07cbbf65100cca5c354662fafe491b269
Code-xed <ngudulu@gmail.com>
2021-11-12 07:16:40 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from .streamsb import StreamSb
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="streamsb",
+    version="0.0.1",
+    author="code_xed",
+    author_email="telegram @necromantik",
+    description="An unoffcial API wrapper for streamsb.com",
+    license="MIT",
+    url="https://github.com/code_xed/streamsb-py",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'aiohttp'
+    ]
+)
