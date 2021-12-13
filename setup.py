30490fb01add658de01501b0b7ba19933b5334f8
Code-xed <ngudulu@gmail.com>
2021-12-13 11:42:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
     install_requires=[
-        'aiohttp'
+        'aiohttp',
+        'requests'
     ]
 )
